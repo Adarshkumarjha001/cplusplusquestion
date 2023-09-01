ba69951ecfabf4f17ea3d8e2a7fca0f5dec3f3a6
Adarsh Kumar Jha <117103488+Adarshkumarjha001@users.noreply.github.com>
2023-09-01 19:49:56 +0530

    01-09-2023
    This is a program of finding maximum value in an array.

---
+++ find_max_value_in_array.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int size;
+	cin>>size;
+	vector<int> ans;
+	vector<int> arr(size,0);
+	for(int i=0;i<size;i++){
+		cin>>arr[i];
+		ans.push_back(arr[i]);
+
+	sort(ans.rbegin(),ans.rend());
+
+	}
+	 cout<<ans[0];
+
+}
+
